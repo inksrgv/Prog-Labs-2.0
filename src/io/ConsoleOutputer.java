3986f11f635ec7921ac361ed5f028341f4b586ff
inksrgv <vragozina393@gmail.com>
2022-02-21 17:48:56 +0300

    Добавила javadoc. Малюсенькие изменения класса ConsoleOutputer

--- src/io/ConsoleOutputer.java
+++ src/io/ConsoleOutputer.java
@@ -1,6 +1,10 @@
 package io;
 import utils.Route;
-public class ConsoleOutputer implements  MessageHandler{
+
+/**
+ * Класс имплементируемый от интерфейса MessageHandler, который позволяет выводить корректные сообщения пользователю на консоль.
+ */
+public class ConsoleOutputer implements MessageHandler{
     public static void output(String msg) {System.out.println(msg); }
     public static void output(Route r){
         System.out.println(r.toString()); //надо посмотреть, мб стоит еще переопределить тустринг
