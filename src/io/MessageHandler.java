6d6539f9a67def3f07c6ad4c7738232f22a1f7a1
inksrgv <vragozina393@gmail.com>
2022-02-21 17:46:53 +0300

    Добавила javadoc

--- src/io/MessageHandler.java
+++ src/io/MessageHandler.java
@@ -1,5 +1,9 @@
 package io;
 import utils.Route;
+
+/**
+ * Интерфейс для вывода на консоль
+ */
 public interface MessageHandler {
     static void output(String msg) {}
     static void output(Route route){}
