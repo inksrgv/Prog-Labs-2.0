bc98e7953de6ac0503366f8fef5d6eb0705259e0
Sofia Fedoteva <93034945+fedoteva@users.noreply.github.com>
2022-02-21 22:49:03 +0300

    Update FileReader.java

--- src/file/FileReader.java
+++ src/file/FileReader.java
@@ -1,5 +1,7 @@
 package file;
 
+import dao.RouteDAO;
+
 import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
@@ -9,22 +11,20 @@ import java.io.FileInputStream;
  */
 public class FileReader {
 
-    public void read(){
+    public void read(RouteDAO routeDAO){
         try {
-            File file = new File("collection.csv");
+            File file = new File("C:\\Users\\Софья\\OneDrive\\Рабочий стол\\collection.csv");
             long l = file.length();
             FileInputStream fis = new FileInputStream(file);
             BufferedInputStream bis = new BufferedInputStream(fis);
-            for(int i = 0; i<l; i++){
-                int data = bis.read();
-                System.out.print((char)data);
-            }
+
             fis.close();
             bis.close();
         }
         catch (Exception e){
             System.out.println(e.getMessage());
         }
+        System.out.println(routeDAO.getDescription().toString());
     }
 
 }
